0fd796eab6290579339d35aff8bb99002e734533
soseoyo12 <songsy0612@gmail.com>
2026-02-05 21:29:06 +0900

    Initial commit

---
+++ PLAM/PLAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLAMApp.swift
+//  PLAM
+//
+//  Created by 송성용 on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct PLAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
